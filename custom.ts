60f37be6c42b917b543d03d3e6bab715b9b088bc
Matt Baker <mbaker@itrsgroup.com>
2024-07-01 21:07:09 +0100

    Created initial timer

---
+++ custom.ts
@@ -0,0 +1,57 @@
+
+/**
+ * Timers
+ */
+//% weight=20 advanced=true color=#013382 icon="\uf2f2"
+namespace timers {
+    /**
+     * Creates a new timer
+     */
+    //% block
+    //% blockSetVariable=timer
+    //% weight=90
+    //% blockGap=25
+    export function createTimer(): Timer {
+        return new Timer()
+    }
+}
+
+//% blockNamespace=timers
+class Timer {
+    private total: number
+
+    public constructor() {
+        this.total = 0
+    }
+
+    //% block
+    //% weight=75
+    //% blockGap=8
+    public start(): void {
+    }
+
+    //% block
+    //% weight=74
+    //% blockGap=8
+    public stop(): void {
+    }
+
+    //% block
+    //% weight=73
+    //% blockGap=25
+    public reset(): void {
+    }
+
+    //% block="millis (ms)"
+    //% blockCombine
+    //% weight=50
+    get millis(): number {
+        return this.total
+    }
+
+    //% blockCombine
+    //% weight=50
+    get seconds(): number {
+        return this.total / 1000
+    }
+}
