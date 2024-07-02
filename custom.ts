ba7baf3bfaff9fff02dcd14446bd436afe2184f4
Matt Baker <mbaker@itrsgroup.com>
2024-07-02 21:01:40 +0100

    Calculate the time it's been running for

--- custom.ts
+++ custom.ts
@@ -19,38 +19,47 @@ namespace timers {
 //% blockNamespace=timers
 class Timer {
     private total: number
+    private startTime: number
 
     public constructor() {
-        this.total = 0
+        this.reset()
     }
 
     //% block
     //% weight=75
     //% blockGap=8
     public start(): void {
+        if (this.startTime === undefined)
+            this.startTime = control.millis()
     }
 
     //% block
     //% weight=74
     //% blockGap=8
     public stop(): void {
+        if (this.startTime !== undefined) {
+            this.total += control.millis() - this.startTime
+            this.startTime = undefined
+        }
     }
 
     //% block
     //% weight=73
     //% blockGap=25
     public reset(): void {
+        this.total = 0
+        this.startTime = undefined
     }
 
     //% blockCombine
     //% weight=50
     get milliseconds(): number {
-        return this.total
+        return this.total + (this.startTime !== undefined ? control.millis() - this.startTime : 0)
     }
 
     //% blockCombine
     //% weight=50
     get seconds(): number {
-        return this.total / 1000
+        return this.milliseconds / 1000
     }
 }
